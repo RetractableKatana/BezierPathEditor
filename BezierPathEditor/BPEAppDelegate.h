5a41b9039090dd2e451a3fd072f769d844849766
James Thompson <retractable.katana@gmail.com>
2014-09-01 13:48:33 -0700

    Hooked up an AppDelegate.
    * Implementing applicationDidFinishLaunching to provide a point for some initial, one time configuration.

---
+++ BezierPathEditor/BPEAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  BPEAppDelegate.h
+//  BezierPathEditor
+//
+//  Created by James Thompson on 9/1/14.
+//  Copyright (c) 2014 IntelligentSprite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BPEAppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ BezierPathEditor/BPEAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  BPEAppDelegate.m
+//  BezierPathEditor
+//
+//  Created by James Thompson on 9/1/14.
+//  Copyright (c) 2014 IntelligentSprite. All rights reserved.
+//
+
+#import "BPEAppDelegate.h"
+
+@implementation BPEAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    // Initialize some values once per app life cycle.
+    [NSColorPanel sharedColorPanel].showsAlpha = YES;
+}
+
+@end
